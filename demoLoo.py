3d888af028b67fe2a1f298589b5beed22c058c3e
Fastone <kms81yj@naver.com>
2024-10-22 09:37:27 +0900

    Day2

--- demoLoo.py
+++ demoLoo.py
@@ -1,4 +1,49 @@
-lst = [100, 200, 300]
+lst  = [100,200,300]
 
-for items in lst:
-    print(items)
+for item in lst:
+    print(item)
+
+
+print("==range()==")
+print(list(range(10)))
+print(list(range(2000,2025)))
+print(list(range(1,32)))
+
+
+
+list("--리스트 내장 ---")
+lst = list(range(1,11))
+print(lst)
+print([i**2 for i in lst if i>5])
+
+
+tp = ("apple", "orrange", "kiwi")
+print([len(i) for i in tp])
+
+d = {100:"apple", 200:"orange"}
+print([v.upper() for v in d.values()])
+
+
+print("--filtering function --")
+
+lst = [10,25,30]
+itemL = filter(None,lst)
+for i in itemL:
+    print("item:{0}".format(i))
+
+def getbiggerthan20(i):
+    return i>20
+
+print("==f==")
+itemL = filter(getbiggerthan20,lst)
+for i in itemL:
+    print("item:{0}".format(i))
+
+
+
+print("==lambda==")
+itemL = filter(lambda x: x>27,lst)
+for i in itemL:
+    print("item:{0}".format(i))
+
+
--- ifelse02.py
+++ ifelse02.py
@@ -4,3 +4,13 @@ if a and 10 / a:
 else:
     print("에러 없이 통과")
     
+
+
+d = {100:"apple", 200:"banana", 300:"orange"}
+for v,s in d.items():
+    print(v,s)
+
+
+lst =[1,2,3,4,5]
+[i**2 for i in lst]
+
